2ddefbe57124f50f288e4cfd497e6b6f6eaaba2c
slaclau <77557628+slaclau@users.noreply.github.com>
2025-11-12 13:02:07 +0000

    Fix totalTiles calculation to use constrained tiles

--- rio_rgbify/mbtiler.py
+++ rio_rgbify/mbtiler.py
@@ -414,7 +414,7 @@ class RGBTiler:
             tiles = _make_tiles(constrained_bbox, "EPSG:4326", self.min_z, self.max_z)
 
         tilesCount = 0
-        totalTiles = len(list(mercantile.tiles(*bbox, range(self.min_z, self.max_z + 1))))
+        totalTiles = len(list(tiles))
         print(f"making {totalTiles} tiles")
         for tile, contents in tqdm.tqdm(self.pool.imap_unordered(self.run_function, tiles), total=totalTiles):
             x, y, z = tile
